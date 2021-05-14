a06fd6f5d7f23869e36c92d77b7abfd87744b054
ŞEYHMUS ASKAR <seyhmus.as@gmail.com>
2021-05-14 16:20:08 +0300

    login commit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { ToastrModule } from "ngx-toastr";
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
 	declarations: [
@@ -26,6 +27,7 @@ import { ProductAddComponent } from './components/product-add/product-add.compon
 		FilterPipePipe,
 		CartSummaryComponent,
 		ProductAddComponent,
+		LoginComponent
 	],
 	imports: [
 		BrowserModule,
